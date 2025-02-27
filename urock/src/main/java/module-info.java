1836e344c8909b53cf8d5490fcbad6e6d4b9add0
CTFerguson2 <143565011+CTFerguson2@users.noreply.github.com>
2025-02-26 20:49:28 -0500

    Added JFugue dependency

--- urock/src/main/java/module-info.java
+++ urock/src/main/java/module-info.java
@@ -2,10 +2,13 @@ module com.urock {
     requires javafx.controls;
     requires javafx.fxml;
     requires json.simple;
+    requires jfugue;
+    
 
     opens com.urock to javafx.fxml;
     exports com.urock;
 
     opens com.model to javafx.fxml;
     exports com.model;
+
 }
