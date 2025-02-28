eaa149d4888ad8b93112e9515481c7c5d3b12aaa
brenskrz <skrzynib@email.sc.edu>
2025-02-28 13:21:04 -0500

    Stubs for #1 , #3 , #5, #8 , #10 , #12 , #15 , #20 , #23 , #28 , #31 , ##36 , #39, #42 , #45 , #47 , #48 , #49 , #50 , #51 , #52 , #54 , #55 , #57 , #58 , #60 , #65 , #66 , #67 ,  #68

---
+++ urock/src/main/java/com/model/LessonList.java
@@ -0,0 +1,25 @@
+package com.model;
+
+import java.util.ArrayList;
+
+public class LessonList {
+    
+    private static LessonList lessonList;
+    private ArrayList<Lesson> lessons;
+
+    private LessonList() {
+
+    }
+
+    public static UserList getInstance() {
+
+    }
+
+    public Lesson getLesson(String title) {
+        return Lesson;
+    }
+
+    public Lesson addLesson(String title) {
+        
+    }
+}
---
+++ urock/src/main/java/com/model/SongList.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class SongList {
+    
+    private static SongList songList;
+    private ArrayList<Song> songs;
+
+    private SongList() {
+        
+    }
+
+    public static SongList getInstance() {
+
+    }
+
+    public ArrayList<Song> getSongList() {
+
+    }
+
+    public boolean loadSong(Song s) {
+        return true;
+    }
+
+    public Song getSongByTitle(String name) {
+
+    }
+
+    public ArrayList<Song> getSongsByDifficulty(DiffultyLevel dv) {
+
+    }
+
+    public ArrayList<Song> getSongsByKey(Key k) {
+
+    }
+
+    public ArrayList<Song> getSongsByGenre(String genre) {
+
+    }
+
+    public void addSong(Song s) {
+
+    }
+}
