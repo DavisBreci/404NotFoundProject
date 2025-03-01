df60c93951af700993fa64dafbf0a2f369250a3d
CTFerguson2 <143565011+CTFerguson2@users.noreply.github.com>
2025-02-28 21:47:41 -0500

    Added stubs for #93, #95, 96, #97, #99, and #100

--- src/main/java/com/model/Score.java
+++ src/main/java/com/model/Score.java
@@ -1,5 +1,46 @@
 package com.model;
 
+import java.util.ArrayList;
+
 public class Score {
- 
+    private ID uuid;
+    private Instrument instrument;
+    private ArrayList<Measure> measures;
+    private int tempo;
+
+    public Score(String uuid, Instrument instrument){
+
+    }
+
+    public void add(Measure m){
+
+    }
+
+    public void add(int index, Measure m){
+
+    }
+
+    public boolean contains(Measure m){
+        return true;
+    }
+
+    public int indexOf(Measure m){
+        return 0;
+    }
+
+    public boolean remove(int index){
+        return true;
+    }
+
+    public boolean remove(Measure m){
+        return true;
+    }
+    
+    public int size(){
+        return 0;
+    }
+
+    public String toString(){
+        return "";
+    }
 }
