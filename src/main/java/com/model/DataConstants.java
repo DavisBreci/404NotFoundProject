1b6f7006ef26c77e2b78fcec4e9997cb61cbd667
brenskrz <skrzynib@email.sc.edu>
2025-03-03 23:31:48 -0500

    Attempt at DataWriter and DataConstants

---
+++ src/main/java/com/model/DataConstants.java
@@ -0,0 +1,44 @@
+package main.java.com.model;
+
+
+
+public class DataConstants {
+    protected static final String USER_FILE_NAME = "JSON/users.jason";
+    // protected static final String USER_TEMP_FILE_NAME = "JSON/users.jason";
+    protected static final String USER_ID = "id";
+    protected static final String USER_USERNAME = "username";
+    protected static final String USER_PASSWORD = "password";
+    protected static final String USER_EMAIL = "email";
+    protected static final String USER_FIRST_NAME = "firstname";
+    protected static final String USER_LAST_NAME = "lastname";
+    protected static final String USER_STREAK = "streak";
+    protected static final String USER_SONGS_PLAYED = "songsPlayed";
+    protected static final String USER_ASSIGNED_LESSONS = "assignedLessons";
+    protected static final String USER_PLAYLISTS = "playlists";
+
+
+    protected static final String SONG_FILE_NAME = "JSON/songs.jason";
+    protected static final String SONG_ID = "id";
+    protected static final String SONG_ARTIST = "artist";
+    protected static final String SONG_GENRE = "genre";
+    protected static final String SONG_DIFFICULTY_LEVEL = "BEGINNER";
+    protected static final String SONG_KEY = "GMAJOR_EMINOR";
+    protected static final String SONG_INSTRUMENT = "guitar";
+    protected static final String SONG_SCORE = "id";
+
+
+    protected static final String PLAYLIST_FILE_NAME = "JSON/playlists.jason";
+    protected static final String PLAYLIST_ID = "id";
+    protected static final String PLAYLIST_TITLE = "title";
+    protected static final String PLAYLIST_AUTHOR = "author";
+    protected static final String PLAYLIST_DESCRIPTION = "description";
+    protected static final String PLAYLIST_SONGS = "songs";
+
+
+    protected static final String LESSONS_FILE_NAME = "JSON/lessons.jason";
+    protected static final String LESSONS_ID = "id";
+    protected static final String LESSONS_SONGS = "songs";
+    protected static final String LESSONS_TITLE = "title";
+
+
+}
--- src/main/java/com/model/DataWriter.java
+++ src/main/java/com/model/DataWriter.java
@@ -1,14 +1,90 @@
 package com.model;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
-public class DataWriter {
-    public boolean saveUsers() {
-        return true;
+
+
+
+public class DataWriter extends DataConstants {
+    public static void saveUsers() {
+        Users users = Users.getInstance();
+        ArrayList<User> userList = users.getUsers();
+
+        // ArrayList<User> userList = new ArrayList();
+        // userList.add(new User(null, "brendan", "skrzyniarz", "brenskrz@gmail.com", "brenskrz", "hello", "2", "3", "id", "id", "monday" ));
+        JSONArray jsonUsers = new JSONArray();
+
+        for(int i = 0; i<users.size(); i++) {
+            jsonUsers.add(getUserJSON(users.get(i)));
+        }
+
+        try (FileWriter file = new FileWriter(USER_FILE_NAME)) {
+            file.write(jsonUsers.toJSONString());
+            file.flush();
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+    public static JSONObject getUserJSON(User user) {
+        JSONObject userDetails = new JSONObject();
+        userDetails.put(USER_ID, user.getId().toString());
+        userDetails.put(USER_USERNAME, user.getUsername());
+        userDetails.put(USER_PASSWORD, user.getPassword());
+        userDetails.put(USER_EMAIL, user.getEmail());
+        userDetails.put(USER_FIRST_NAME, user.getFirstName());
+        userDetails.put(USER_LAST_NAME, user.getLastName());
+        userDetails.put(USER_STREAK, user.getStreak());
+        userDetails.put(USER_SONGS_PLAYED, user.getSongsPlayed());
+        userDetails.put(USER_ASSIGNED_LESSONS, user.getAssignedLessons());
+        userDetails.put(USER_PLAYLISTS, user.getPlaylists());
+
+        return userDetails;
     }
 
-    public boolean saveSongs() {
-        return true;
+    public static void saveSongs() {
+        Songs songs = Songs.getInstance();
+        ArrayList<Song> songList = songs.getSongs();
+
+        // ArrayList<User> userList = new ArrayList();
+        // userList.add(new User(null, "brendan", "skrzyniarz", "brenskrz@gmail.com", "brenskrz", "hello", "2", "3", "id", "id", "monday" ));
+        JSONArray jsonSongs = new JSONArray();
+
+        for(int i = 0; i<songs.size(); i++) {
+            jsonSongs.add(getSongJSON(users.get(i)));
+        }
+
+        try (FileWriter file = new FileWriter(SONG_FILE_NAME)) {
+            file.write(jsonSongs.toJSONString());
+            file.flush();
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    public static JSONObject getSongJSON(Song song) {
+        JSONObject songDetails = new JSONObject();
+
+        songDetails.put(SONG_ID, song.getId().toString());
+        songDetails.put(SONG_ARTIST, song.getArtist());
+        songDetails.put(SONG_GENRE, song.getGenre());
+        songDetails.put(SONG_DIFFICULTY_LEVEL, song.getDifficultyLevel());
+        songDetails.put(SONG_KEY, song.getKey());
+        songDetails.put(SONG_INSTRUMENT, song.getInstrument());
+        songDetails.put(SONG_SCORE, song.getScore());
+
+        return songDetails;
+    }
+    
 
     public boolean savePlaylists() {
         return true;
