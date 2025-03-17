f4bea539651917eea83149f61fae44e05f09ce8d
ryanMazz <mazzellr@email.sc.edu>
2025-03-17 01:49:08 -0400

    #1 progress and fixes, #44 progress. getUsers & getTeachers functional minus playlists and lessons. #180 progress, Scores load.

--- src/main/java/com/model/DataConstants.java
+++ src/main/java/com/model/DataConstants.java
@@ -1,4 +1,4 @@
-package main.java.com.model;
+package com.model;
 
 
 
@@ -9,8 +9,8 @@ public class DataConstants {
     protected static final String USER_USERNAME = "username";
     protected static final String USER_PASSWORD = "password";
     protected static final String USER_EMAIL = "email";
-    protected static final String USER_FIRST_NAME = "firstname";
-    protected static final String USER_LAST_NAME = "lastname";
+    protected static final String USER_FIRST_NAME = "firstName";
+    protected static final String USER_LAST_NAME = "lastName";
     protected static final String USER_STREAK = "streak";
     protected static final String USER_SONGS_PLAYED = "songsPlayed";
     protected static final String USER_ASSIGNED_LESSONS = "assignedLessons";
@@ -28,8 +28,7 @@ public class DataConstants {
     protected static final String SONG_DIFFICULTY_LEVEL = "BEGINNER";
     protected static final String SONG_KEY = "GMAJOR_EMINOR";
     protected static final String SONG_INSTRUMENT = "guitar";
-    protected static final String SONG_SCORE = "id";
-
+    protected static final String SONG_SCORE = "score";
 
     protected static final String PLAYLIST_FILE_NAME = "JSON/playlists.json";
     protected static final String PLAYLIST_ID = "id";
@@ -38,11 +37,34 @@ public class DataConstants {
     protected static final String PLAYLIST_DESCRIPTION = "description";
     protected static final String PLAYLIST_SONGS = "songs";
 
-
     protected static final String LESSONS_FILE_NAME = "JSON/lessons.json";
     protected static final String LESSONS_ID = "id";
     protected static final String LESSONS_SONGS = "songs";
     protected static final String LESSONS_TITLE = "title";
 
+    /* 
+     * Additional data constants
+     * @author ryanMazz
+     */
+    protected static final String SCORE_FILE_NAME = "JSON/scores.json";
+    protected static final String SCORE_ID = "uuid";
+    protected static final String SCORE_INSTRUMENT = "instrument";
+    protected static final String SCORE_TEMPO = "tempo";
+    protected static final String SCORE_MEASURES = "measures";
 
+    protected static final String MEASURE_TIME_SIGNATURE = "timeSignature";
+    protected static final String MEASURE_CHORDS = "chords";
+
+    protected static final String CHORD_OFFSET = "offset";
+    protected static final String CHORD_VALUE = "value";
+    protected static final String CHORD_DOTTED = "dotted";
+    protected static final String CHORD_NOTES = "notes";
+
+    protected static final String NOTE_PITCH_CLASS = "pitchClass";
+    protected static final String NOTE_OCTAVE = "octave"; 
+    protected static final String NOTE_STRING_POSITION = "string";
+    protected static final String NOTE_FRONT_TIE = "frontTie";
+    protected static final String NOTE_BACK_TIE = "backTie";
+
+    
 }
