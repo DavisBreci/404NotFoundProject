cb38a65e48acd9336b2e9b43d14e467116e27e32
CTFerguson2 <143565011+CTFerguson2@users.noreply.github.com>
2025-03-20 12:56:03 -0400

    Fixed score JSON

--- src/main/java/com/model/TempTester.java
+++ src/main/java/com/model/TempTester.java
@@ -14,21 +14,21 @@ public class TempTester {
         //Loading a score from a JSON file
         Score gMajChord = DataLoader.getScoreFromID("a42d710f-afcb-4bce-bfd7-ecb43e6a5a89");
         p.play(gMajChord.getSequence(0, gMajChord.size(), null, 1));
-        // Loading a score w/out chords from a MIDI file
-        rawMidi = Score.loadSequence("src\\main\\midi\\Teen_Town.mid");
-        Score teenTown = Score.midiToScore(rawMidi, 0, Instrument.FRETLESS_BASS);
-        p.play(teenTown.getSequence(0, teenTown.size(), null, 1));
-        // Loading a chordal score from a MIDI file        
-        rawMidi = Score.loadSequence("src\\main\\midi\\Larks_II_GuitarOnly.mid");
-        Score ltia = Score.midiToScore(rawMidi, 0, Instrument.DISTORTION_GUITAR);
-        p.play(ltia.getSequence(0, ltia.size(), null, 1));
-        ArrayList<User> test = new ArrayList<User>();
-        test.sort(new Comparator<User>(){
-            @Override
-            public int compare(User o1, User o2) {
-                return o1.getFirstName().compareTo(o2.getFirstName());
-            }
-        }
-        );
+        // // Loading a score w/out chords from a MIDI file
+        // rawMidi = Score.loadSequence("src\\main\\midi\\Teen_Town.mid");
+        // Score teenTown = Score.midiToScore(rawMidi, 0, Instrument.FRETLESS_BASS);
+        // p.play(teenTown.getSequence(0, teenTown.size(), null, 1));
+        // // Loading a chordal score from a MIDI file        
+        // rawMidi = Score.loadSequence("src\\main\\midi\\Larks_II_GuitarOnly.mid");
+        // Score ltia = Score.midiToScore(rawMidi, 0, Instrument.DISTORTION_GUITAR);
+        // p.play(ltia.getSequence(0, ltia.size(), null, 1));
+        // ArrayList<User> test = new ArrayList<User>();
+        // test.sort(new Comparator<User>(){
+        //     @Override
+        //     public int compare(User o1, User o2) {
+        //         return o1.getFirstName().compareTo(o2.getFirstName());
+        //     }
+        // }
+        // );
     }
 }
