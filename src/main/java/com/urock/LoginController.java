4ab05913ba541e7d2a397f61e59bccaaee135df4
DavisBreci <davisbreci@outlook.com>
2025-04-10 13:00:04 -0400

    login page of GUI functions kinda

---
+++ src/main/java/com/urock/LoginController.java
@@ -0,0 +1,38 @@
+package com.urock;
+
+import javafx.scene.control.TextField;
+
+import com.model.MusicSystemFACADE;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
+import model.*;
+
+public class LoginController {
+    String username;
+    String password;
+    boolean teacher;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private TextField password_txt;
+
+    @FXML
+    private ToggleButton teacherToggle;
+
+    @FXML
+    private TextField username_txt;
+
+    @FXML
+    void login(ActionEvent event) {
+        username = username_txt.getText();
+        password = password_txt.getText();
+        teacher = teacherToggle.isSelected();
+        MusicSystemFACADE facade = MusicSystemFACADE.getInstance();
+        if(facade.login(username, password))
+            System.out.println("le epic login complete");
+    }
+
+}
