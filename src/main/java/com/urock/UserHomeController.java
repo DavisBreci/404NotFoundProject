552afbae24516d9e4be736959a4ae37695c40042
CTFerguson2 <143565011+CTFerguson2@users.noreply.github.com>
2025-04-13 20:24:55 -0400

    Created UserHomeController stub

--- src/main/java/com/urock/UserHomeController.java
+++ src/main/java/com/urock/UserHomeController.java
@@ -1,5 +1,28 @@
 package com.urock;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
+
 public class UserHomeController {
-    
+
+    @FXML
+    private Button buttonCompose;
+
+    @FXML
+    private Button buttonSearch;
+
+    @FXML
+    private ScrollPane scrollLessons;
+
+    @FXML
+    private HBox scrollLessonsContent;
+
+    @FXML
+    private ScrollPane scrollPlaylists;
+
+    @FXML
+    private HBox scrollPlaylistsContent;
+
 }
